1ca21d06436290aaa446158d4d70f4a91d693122
ebram baratiii <99908052+ebiii-m@users.noreply.github.com>
2025-04-24 19:55:40 +0330

    new changes

--- dbconnect/src/lib.rs
+++ dbconnect/src/lib.rs
@@ -1,12 +1,17 @@
+use once_cell::sync::OnceCell;
+use std::sync::OnceLock;
+
+
 pub mod get_data;
 pub mod set_data;
 pub mod find;
 pub mod group;
 
 pub mod connect {
+    #[derive(Debug)]
     pub struct DBConf {
-        server: String,
-        port: u32,
+        pub server: String,
+        pub port: u32,
     }
 
     impl DBConf {
@@ -16,6 +21,35 @@ pub mod connect {
                 port: port,
             }
         }
+
+        pub fn get_server(&self) -> String {
+            format!("{}:{}", &self.server, &self.port)
+        }
+    }
+}
+
+static CONFIG: OnceCell<connect::DBConf> = OnceCell::new();
+
+pub fn set_config(conf: connect::DBConf) -> Result<(), &'static str> {
+    CONFIG
+        .set(conf)
+        .map_err(|_| "Config can only be set once!")
+}
+
+pub fn get_config() -> String {
+    CONFIG.get().expect("Config not set!").get_server()
+}
+
+mod api_response {
+    use serde_json::Value;
+    use serde::{ Deserialize , Serialize };
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct ApiResponse {
+        status: String,
+        message: Option<String>,
+        rec_id: Option<String>,
+        data: Option<Value>,
     }
 }
 
