21337e2ab1996a423c39387f0ae06a55bbd5bc25
ebram baratiii <99908052+ebiii-m@users.noreply.github.com>
2025-04-26 10:12:15 +0330

    new changes

--- configs/src/blogs/mainpages.rs
+++ configs/src/blogs/mainpages.rs
@@ -2,10 +2,9 @@ use super::super::config_server::Template;
 use super::super::render::render_page;
 use actix_web::{ Responder , HttpResponse , get};
 use dbconnect::get_data;
-use super::config_server::Template;
 
 #[get("/{id}/")]
 pub async fn page(id: String) -> impl Responder {
-    let template = render_page(get_data::get(id))?;
+    let template = render_page(get_data::get_rec(id))?;
     HttpResponse::Ok().content_type("text/html").body(template)
 }
--- configs/src/blogs/post.rs
+++ configs/src/blogs/post.rs
@@ -1,11 +1,10 @@
-use super::Page;
 use super::super::render::render_post;
 use actix_web::{ Responder , HttpResponse , get};
 use dbconnect::get_data;
-use super::config_server::Template;
+use super::super::config_server::Template;
 
 #[get("/{id}/")]
-pub async fn post(id: String) -> Responder {
-    let template = render_post(get_data(id))?;
+pub async fn post(id: String) -> impl Responder {
+    let template = render_post(get_data::get_rec(id))?;
     HttpResponse::Ok().content_type("text/html").body(template)
 }
